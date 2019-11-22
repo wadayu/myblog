c12fb2a4eb023a78389c6f028a6b813fa6fd5d6e
wangdaoyun <wangdaoyun@zhiyihealth.com.cn>
2019-11-22 20:24:28 +0800

    create apps and models

--- myblog/settings/develop.py
+++ myblog/settings/develop.py
@@ -4,8 +4,13 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        'NAME': 'myblog',         # 你要存储数据的库名，事先要创建之
+        'USER': 'root',         # 数据库用户名
+        'PASSWORD': '123456',     # 密码
+        'HOST': '192.168.14.40',    # 主机
+        'PORT': '3306',         # 数据库使用的端口
+        # 'TEST': {'NAME': 'testcase_db'}
     }
 }
 
